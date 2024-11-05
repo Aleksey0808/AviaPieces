491e5e9d76242d68956bd8ddf32c9e0ed43e2f73
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2024-11-05 13:24:57 +0200

    Basic settings and project structure

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { View, Modal, Image, TouchableOpacity, Text, StyleSheet, } from 'react-native';
+// import { NativeRouter } from "react-router-native";
+// import { NavigationContainer } from '@react-navigation/native';
+// import AppNavigator from './src/navigations/AppNavigator';
+
+export default function App() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Avia Pieces</Text>
+      </View>
+    );
+  }
