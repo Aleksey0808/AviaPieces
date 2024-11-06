457e1efc870faf5bf4ab52aa58b7267b76083d29
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2024-11-06 15:58:23 +0200

    add logics

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,31 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, ImageBackground, View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const GameScreen = ({ route }) => {
-  const { puzzle, levelData } = route.params; 
-  const [selectedPiece, setSelectedPiece] = useState(null); 
-  const [grid, setGrid] = useState([]); 
-  const [remainingPieces, setRemainingPieces] = useState(puzzle.pieces); 
-  const [columns, setColumns] = useState(2); 
-  console.log(levelData.level)
+  const { puzzle, levelData } = route.params;
+  const [selectedPiece, setSelectedPiece] = useState(null);
+  const [grid, setGrid] = useState([]);
+  const [remainingPieces, setRemainingPieces] = useState(puzzle.pieces);
+  const [columns, setColumns] = useState(2);
+  const [showSolution, setShowSolution] = useState(false); 
+  const [defaultImg, setDefaultImg] = useState(require('../../assets/images/elements/easyDef.png')); 
 
   useEffect(() => {
-    console.log("Puzzle level:", puzzle);
+    const setupGrid = async () => {
+      const savedGrid = await AsyncStorage.getItem(`savedGrid_${levelData.level}`);
+      if (savedGrid) {
+        setGrid(JSON.parse(savedGrid));
+      } else {
+        const emptyGrid = Array(puzzle.pieces.length).fill(null);
+        setGrid(emptyGrid);
+      }
+    };
+    
     if (levelData.level === 'easy') {
-      console.log('easy')
       setColumns(2);
+      setDefaultImg(require('../../assets/images/elements/easyDef.png'))
     } else if (levelData.level === 'normal') {
-      console.log('normal')
       setColumns(3);
+      setDefaultImg(require('../../assets/images/elements/normalDef.png'))
     } else if (levelData.level === 'hard') {
-      console.log('hard')
       setColumns(4);
+      setDefaultImg(require('../../assets/images/elements/hardDef.png'))
     }
+    
+    setupGrid();
+    
+    setShowSolution(true);
+    setTimeout(() => {
+      setShowSolution(false);
+    }, 3000);
+  }, [puzzle, levelData.level]);
 
-    const numberOfPieces = puzzle.pieces.length;
-    const emptyGrid = Array(numberOfPieces).fill(null); 
-    setGrid(emptyGrid);
-  }, []);
+  const saveGameState = async (newGrid) => {
+    await AsyncStorage.setItem(`savedGrid_${levelData.level}`, JSON.stringify(newGrid));
+  };
 
   const handlePieceSelect = (piece) => {
     setSelectedPiece(piece);
@@ -39,38 +57,44 @@ const GameScreen = ({ route }) => {
 
       const newRemainingPieces = remainingPieces.filter(piece => piece.id !== selectedPiece.id);
       setRemainingPieces(newRemainingPieces);
-
       setSelectedPiece(null);
+
+      saveGameState(newGrid); 
     }
   };
 
-  const renderPuzzlePieces = () => {
-    return (
-      <FlatList
-        data={remainingPieces}
-        horizontal
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handlePieceSelect(item)}>
-            <Image source={item.image} style={styles.pieceImage} />
-          </TouchableOpacity>
-        )}
-        contentContainerStyle={styles.pieceList} 
-      />
-    );
+  const restartGame = () => {
+    const emptyGrid = Array(puzzle.pieces.length).fill(null);
+    setGrid(emptyGrid);
+    setRemainingPieces(puzzle.pieces);
+    setSelectedPiece(null);
+    AsyncStorage.removeItem(`savedGrid_${levelData.level}`);
   };
+
+  const renderPuzzlePieces = () => (
+    <FlatList
+      data={remainingPieces}
+      horizontal
+      keyExtractor={(item, index) => `${index}-${levelData.level}-${new Date().getTime()}`}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => handlePieceSelect(item)}>
+          <Image source={item.image} style={styles.pieceImage} />
+        </TouchableOpacity>
+      )}
+      contentContainerStyle={styles.pieceList}
+    />
+  );
 
   const renderPuzzleGrid = () => {
     const renderItem = ({ item, index }) => (
       <TouchableOpacity
-        key={index}
         style={styles.gridCell}
         onPress={() => handleGridCellPress(index)}
       >
-        {item ? (
-          <Image source={item.image} style={styles.gridCellImage} />
+        {item || showSolution ? (
+          <Image source={item ? item.image : puzzle.pieces[index].image} style={styles.gridCellImage} />
         ) : (
-          <View style={styles.emptyCell} />
+          <Image source={defaultImg} style={styles.gridCellImage} />
         )}
       </TouchableOpacity>
     );
@@ -78,14 +102,21 @@ const GameScreen = ({ route }) => {
     return (
       <FlatList
         data={grid}
-        numColumns={columns} 
+        numColumns={columns}
         renderItem={renderItem}
-        keyExtractor={(item, index) => `${index}-${columns}`}
+        keyExtractor={(item, index) => `${index}-${columns}-${levelData.level}`}
         style={styles.grid}
-        contentContainerStyle={styles.gridContainer} 
-        key={columns}
+        contentContainerStyle={styles.gridContainer}
+        key={`${columns}-${levelData.level}`} 
       />
     );
+  };
+
+  const handleShowSolution = () => {
+    setShowSolution(true);
+    setTimeout(() => {
+      setShowSolution(false);
+    }, 3000);
   };
 
   return (
@@ -95,12 +126,16 @@ const GameScreen = ({ route }) => {
     >
       <View style={styles.container}>
         <Text style={styles.title}>{puzzle.name}</Text>
-
-        {renderPuzzleGrid()} 
-
+        {renderPuzzleGrid()}
         <View style={styles.piecesContainer}>
-          {renderPuzzlePieces()} 
+          {renderPuzzlePieces()}
         </View>
+        <TouchableOpacity style={styles.solutionButton} onPress={handleShowSolution}>
+          <Text style={styles.solutionButtonText}>Show solution</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.restartButton} onPress={restartGame}>
+          <Text style={styles.restartButtonText}>reload game</Text>
+        </TouchableOpacity>
       </View>
     </ImageBackground>
   );
@@ -144,11 +179,11 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   gridContainer: {
-    alignItems: 'center', // Убираем flexWrap
+    alignItems: 'center',
     justifyContent: 'center',
   },
   gridCell: {
-    width: 100, // Подгоните под нужные размеры ячеек
+    width: 100,
     height: 100,
     margin: 5,
     borderWidth: 2,
@@ -162,13 +197,30 @@ const styles = StyleSheet.create({
     height: '100%',
     resizeMode: 'cover',
   },
-  emptyCell: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#eee',
+  solutionButton: {
+    backgroundColor: '#00f',
+    padding: 10,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+  },
+  solutionButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+  },
+  restartButton: {
+    backgroundColor: '#f00',
+    padding: 10,
+    borderRadius: 5,
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+  },
+  restartButtonText: {
+    color: '#FFF',
+    fontSize: 16,
   },
 });
-
-
 
 export default GameScreen;
